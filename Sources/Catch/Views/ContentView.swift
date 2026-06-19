b395407ce95c6ac989168073f1ad054c672cffc4
Tom Brow <tomb@block.xyz>
2026-06-19 05:45:30 -0700

    Remove obsolete SwiftUI preview views

--- Sources/Catch/Views/ContentView.swift
+++ Sources/Catch/Views/ContentView.swift
@@ -25,3 +25,22 @@ struct ContentView: View {
         }
     }
 }
+
+#Preview("Content") {
+    ContentPreviewHost(isTestBuild: false)
+}
+
+#Preview("Content Test Build") {
+    ContentPreviewHost(isTestBuild: true)
+}
+
+private struct ContentPreviewHost: View {
+    @StateObject private var store = SessionStore(appSupportDirectoryName: "CatchPreview")
+    let isTestBuild: Bool
+
+    var body: some View {
+        ContentView(isTestBuild: isTestBuild, keyboardMonitorEnabled: false)
+            .environmentObject(store)
+            .frame(width: 560, height: 430)
+    }
+}
